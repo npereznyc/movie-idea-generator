415906a86181272bbea64419d88eafc0ea1357f0
npereznyc <natalieperez84@gmail.com>
2023-06-06 17:02:05 -0400

    added .env

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const setupTextarea = document.getElementById('setup-textarea') 
-const setupInputContainer = document.getElementById('setup-input-container')
-const movieBossText = document.getElementById('movie-boss-text')
+// const setupTextarea = document.getElementById('setup-textarea') 
+// const setupInputContainer = document.getElementById('setup-input-container')
+// const movieBossText = document.getElementById('movie-boss-text')
 const apiKey = "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"
 const url = "https://api.openai.com/v1/completions"
 
 
-document.getElementById("send-btn").addEventListener("click", () => {
+// document.getElementById("send-btn").addEventListener("click", () => {
   // if (setupTextarea.value) {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
   // }
   fetchBotReply()
-})
+// })
 
 function fetchBotReply(){
   fetch(url, {
